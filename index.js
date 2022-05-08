81af9dd584a39e1573a39439b8cc3ff75bb42124
mdHasif2001 <iamhasib727@gmail.com>
2022-05-09 00:27:23 +0600

    mongodb atlas connected

--- index.js
+++ index.js
@@ -2,21 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// use middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}<password>@cluster0.6esce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xavyl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const inventoryCollection = client.db('Marine').collection('inventory');
+
+        app.get('/inventory', async(req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        })
+
+        app.get('/inventory/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const inventories = await serviceCollection.findOne(query);
+            res.send(inventories);
+        });
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running My Node CRUD Server here we go');
